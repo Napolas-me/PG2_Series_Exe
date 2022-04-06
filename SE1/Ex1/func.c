a301cef5c8ed739140f8fd094b6b22ada6323da1
Pedro Apolinario <pedroapolinariovf@gmail.com>
2022-04-06 15:29:30 +0100

    more updates on exe2

--- SE1/Ex1/func.c
+++ SE1/Ex1/func.c
@@ -1,9 +1,12 @@
 #include "func.h"
 
-int signExtend(int value, int size){
+int signExtend(int value, int size){// size is n x 7
     int result = 0;
+    int testSig = (value >> size) & 0x1;
+    
+    if(testSIg) result = 0xFFFF;// set to max value int
 
-    while(size > 0){
+    while(size > 0){ // not right
         result &= (value & 0x1);
         result <<= 1;
         value >>= 1;
--- SE1/Ex2/stringMan.c
+++ SE1/Ex2/stringMan.c
@@ -1,7 +1,28 @@
 #include "stringMan.h"
+enum stringTypes{
+	SPACE = ' ',
+	TAB = '\t',
+	CHANGE_LINE = '\n',
+	FIELD_SEP = ';'
+};
 
 char *cutEndingSpaces( char *str ){
+	while(*str == SPACE || *str == TAB || *str == SPACE){ // skip aos n-elementos que contenham os tipos a eliminar
+		*str++;
+	}
 
+	if(str[0] == '\0') return str;
+	
+	int sizeString = sizeof(str)/sizeof(str[0]); // tamanho da string aka numero de elementos
+
+	for(int i = sizeString - 1;; i--){
+		if(str[i] != SPACE  && str[i] != TAB && str[i] != CHANGE_LINE){
+			str[i+1] = '\0';
+			break;
+		}
+	}	
+
+	return str;
 }
 
 int fields(char *line, char *ptrs[], int max_fields ){
