b6910e1771a8ff2e18d9852e65fa3c845ca8467c
Napolas-me <pedroapolinariovf@gmail.com>
2022-04-06 19:41:37 +0100

    added test in stringExe, not tested

--- SE1/Ex2/stringExe.c
+++ SE1/Ex2/stringExe.c
@@ -1,5 +1,17 @@
 #include "stringMan.h"
+#include <stdio.h>
+
+char* simpleTestString = "\t Teste\n  ";
 
 int main(int argc, char* argv[]){
+    char* returnTest1 = 0;
 
+    returnTest1 = cutEndingSpaces(simpleTestString);
+    printChanges(simpleTestString, returnTest1);
+    return 0;
+}
+
+void printChanges(char* ini, char* change){
+    printf("%s --> ", ini);
+    printf("%s\n", change);   
 }
