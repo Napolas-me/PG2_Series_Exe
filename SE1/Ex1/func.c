1c92dea67fe6d23843ca36c4e2805e7fc6f9c4b3
seemsLgit <edropcorreia@gmail.com>
2022-04-07 11:08:00 +0100

    func is WORKING!!! also new enunciate

--- SE1/Ex1/func.c
+++ SE1/Ex1/func.c
@@ -1,21 +1,21 @@
 #include "func.h"
 
 int signExtend(int value, int size){// size is n x 7, multiple of 7, size is the number of bits to store
-    int result = 0;
-    int mask = (-1 >> (sizeof(int) - size) );
-    int testSig = (value >> (size + 1)) & 0x1;
     
-    if(testSig)
-        result = -1 | (mask & value);
-    else
-        result = mask & value
-
-    while(size > 0){ // not right
-        result &= (value & 0x1);
-        result <<= 1;
-        value >>= 1;
-        size--;
+    int mask1 = (-1 << size ); //place zeros in the location of value to store with size size
+    //int mask2 = ( -1 >> (sizeof(int) - size) );
+    int mask2=0;
+    
+    for(int i = 0; i <= size - 1; i++) {
+        mask2 = mask2 | 0x1;
+        mask2 = mask2 << 1;
     }
+    mask2 = mask2 | 0x1;
+     
+    int testBitSig = (value >> (size - 1)) & 0x1; 
 
-    return result;
+    if(testBitSig)
+        return (mask1 | value);
+    else
+        return (mask2 & value);
 }
--- SE1/test/test_file.c
+++ SE1/test/test_file.c
@@ -6,13 +6,55 @@
 #define DATA_MASK (~(~0 << DATA_SIZE)) // ~FINAL_MASK
 #define SIGN_SIZE 1
 
+int signExtend(int value, int size){// size is n x 7, multiple of 7, size is the number of bits to store
+    
+    int mask1 = (-1 << size ); //place zeros in the location of value to store with size size
+    //int mask2 = ( -1 >> (sizeof(int) - size) );
+    int mask2=0;
+    int testBitSig = (value >> (size - 1)) & 0x1; 
+
+    for(int i = 0; i <= size - 1; i++) {
+        mask2 = mask2 | 0x1;
+        mask2 = mask2 << 1;
+    }
+    mask2 = mask2 | 0x1;
+     
+    if(testBitSig)
+        return (mask1 | value);
+    else
+        return (mask2 & value);
+}
+
+
 int main( int argc, char *argv[] ){
 
         printf("\n --- vaue of CHAR_BIT %d ", CHAR_BIT);
         printf("\n --- vaue of DATA_SIZE %d", DATA_SIZE);
         printf("\n --- vaue of FINAL_MASK %x ", FINAL_MASK);
         printf("\n --- vaue of DATA_MASK %x ", DATA_MASK);
-        printf("\n --- vaue of DATA_MASK %x ", DATA_MASK);
+        printf("\n --- vaue of DATA_MASK %x \n", DATA_MASK);
+
+
+        int size = 5;
+        int value = 0b01111;
+
+        int mask1 = (-1 << size );
+        int mask2 = 0;
+
+        for(int i = 0; i < size-1; i++) {
+            mask2 = mask2 | 0x1;
+            mask2 = mask2 << 1;
+        } 
+        mask2 = mask2 | 0x1;
+
+        printf("\n --- mask1 is %x \n", mask1);
+        printf("\n --- mask2 is %x \n", mask2);
+
+        int result = signExtend(value, size);
+        printf("\n --- Result in hex is %x \n", result);
+        printf("\n --- Result in dec is %d \n", result);
+
+
 
     return 0;
 }
