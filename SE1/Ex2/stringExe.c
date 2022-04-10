98883234543d1183a18cafb1751882677ea86e2a
Napolas-me <pedroapolinariovf@gmail.com>
2022-04-10 21:17:21 +0100

    fuck sizeof

--- SE1/Ex2/stringExe.c
+++ SE1/Ex2/stringExe.c
@@ -1,13 +1,25 @@
 #include "stringMan.h"
 #include <stdio.h>
 
+#define MAX_SIZE 4
+
+void printChanges(char* ini, char* change);
+
 char* simpleTestString = "\t Teste\n  ";
+char* complexExample = "primeiro campo;; terceiro campo \t; ; palavras do quinto campo 1234\n";
 
 int main(int argc, char* argv[]){
     char* returnTest1 = 0;
+    char* returnTest2[MAX_SIZE];
 
     returnTest1 = cutEndingSpaces(simpleTestString);
     printChanges(simpleTestString, returnTest1);
+
+    /*int numFields = fields(complexExample, returnTest2, MAX_SIZE);
+
+    for(int i = 0; i < MAX_SIZE; i++) printf("ptrs[%d] = %s\n", i, returnTest2[i]);
+    printf("The number of fields was %d", numFields);*/
+
     return 0;
 }
 
--- SE1/Ex2/stringMan.c
+++ SE1/Ex2/stringMan.c
@@ -1,27 +1,74 @@
 #include "stringMan.h"
 #include <string.h>
+#include <stdio.h>
+
+//#define	SPACE ' '
+//#define	TAB '\t'
+//#define	CHANGE_LINE '\n'
+//#define	FIELD_SEP ';'
+
+#define MAX_CHAR_VAL 2
 
 enum stringTypes{
-	SPACE = ' ',
-	TAB = '\t',
-	CHANGE_LINE = '\n',
-	FIELD_SEP = ';'
+    SPACE = ' ',
+    TAB = '\t',
+    CHANGE_LINE = '\n'
 };
 
+char FIELD_SEP[MAX_CHAR_VAL] = ";";
+
 char *cutEndingSpaces( char *str ){
-	while(*str == SPACE || *str == TAB || *str == SPACE) *str++; // skip aos n-elementos que contenham os tipos a eliminar
+	
+	while(*str == SPACE || *str == TAB || *str == CHANGE_LINE) *str++; // skip aos n-elementos que contenham os tipos a eliminar
 
 	if(str[0] == '\0') return str;
-	
-	int sizeString = sizeof(str)/sizeof(str[0]); // tamanho da string aka numero de elementos
+
+	//printf("Cur Array -> (%s)\n", str);
+
+	//char* aux = str;
+
+	//printf("checkpoint 1\n");
+	int times = 0;
+	while(*str != SPACE && *str != TAB && *str != CHANGE_LINE){
+		//printf("%s", str);
+		times++;
+		*str++;
+	}
+	printf("times = %d\n", times);
+
+	printf("str last char-> (%s)\n", str);
+
+	if(*str == '\0') return str;
+	else str = '\0';
+
+	//printf("checkpoint 2\n");
+
+	while(times > 0){
+		printf("1");
+		*str--;
+		times--;
+	}
+
+	printf("Cur Array -> (%s)\n", str);
+
+	//right till here
+	//printf("sizeofString = %ld", sizeof(*str));
+	//printf("sizeofString[0] = %ld", sizeof(str[0]));
+
+	/*int sizeString = 0 ;//sizeof(str)/sizeof(str[0]); // tamanho da string aka numero de elementos
+	for(int i = 0; aux[i] != '\0'; i++) ++sizeString;
+
+	printf("sizeString = %d", sizeString);
 
 	for(int i = sizeString - 1;; i--){
+		printf("iter %d/n", i);
+
 		if(str[i] != SPACE || str[i] != TAB || str[i] != CHANGE_LINE){
 			str[i+1] = '\0';
 			break;
 		}
-	}	
-
+	}*/
+	printf("checkpoint 3\n");
 	return str;
 }
 
@@ -34,31 +81,19 @@ int fields(char *line, char *ptrs[], int max_fields ){
 	int sizeString = sizeof(line)/sizeof(*line); // Tamanho de line
 
 	// Contar a quantidade de ";" existentes em line
-	//comprimi a contagem para dentro de um unico for
 	
 	for(int i = 0; i <= sizeString; i++) {
-		if(line[i] == FIELD_SEP || line[i] == '\0') fields++;
+		if(line[i] == FIELD_SEP[0] || line[i] == '\0') fields++;
 	}
-
-	// Existe sempre +1 campo do que ";"
-	// Em principio esta corrigido no if em cima
-	// if (fields > 0) fields++;
 		
 	/* Separar os campos */
 		
 	char *token = strtok(line, FIELD_SEP); //"breaks string into a series of tokens using the delimiter"
-	//fields++;
 	
 	for(int i = 0; i < max_fields; i++){	
 		ptrs[i] = cutEndingSpaces(token);
 		token = strtok(NULL, FIELD_SEP); // Usamos o NULL para indicar a função que a pesquisa deve continuar no ponteiro anterior 
 	}
-		
-	/* limpar os campos */
-
-	// retirar espaços usando a função cutEndingSpaces
-	//reduçao de numero de FORs	
-	//for(int i = 0; i < max_fields ; i++) ptrs[i] = cutEndingSpaces(ptrs[i]);
 	
 	return fields;
 }
